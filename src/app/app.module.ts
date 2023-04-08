e68439108c34b8e7824b7c6e2e19100e0859d726
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2023-04-07 20:18:54 -0400

    added login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ApolloModule, APOLLO_OPTIONS, } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { InMemoryCache } from '@apollo/client/core';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from "@angular/router";
+
+const routes: Routes = [
+  
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    ApolloModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  exports: [RouterModule],
+  providers: [
+    {
+        provide: APOLLO_OPTIONS,
+        useFactory(httpLink: HttpLink) {
+            return {
+                cache: new InMemoryCache(),
+                link: httpLink.create({
+                    uri: "https://elizaveta.warptower.outerwilds.net/graphql",
+                }),
+            };
+        },
+        deps: [HttpLink],
+    },
+],
+bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  loginForm = new FormGroup({
+    username: new FormControl("", Validators.required),
+    password: new FormControl("", Validators.required)
+  });
+  
+  errorMessage!: String
+
+  constructor(private apollo: Apollo, private router: Router) { }
+
+  async login() {
+    // Send the login request to the server
+    await this.apollo.query({
+      query: gql`
+      query {
+        login(username: "${this.loginForm.value.username}", password: "${this.loginForm.value.password}")
+      }
+        `
+    }).subscribe(result => {
+      // @ts-expect-error if wrong password
+      if (result.data.login === "Invalid username or password") {
+        this.errorMessage = "Invalid username or password"
+      }
+      else {
+        // redirect to the homepage
+        this.router.navigate(["/homepage"]);
+      }
+    });
+  }
+}
