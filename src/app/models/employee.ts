43d7eaad3cbc8ccef2f4d3405488599765e49039
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2023-04-08 19:58:25 -0400

    removed uneccessary stuff

--- src/app/models/employee.ts
+++ src/app/models/employee.ts
@@ -1,18 +1,18 @@
 export class Employee {
-    id: number;
-    firstname: string;
-    lastname: string;
-    email: string;
-    gender: string;
-    salary: number;
-    onClick?: () => void; // Add a property for the click event
-    
-    constructor(id: number, firstname: string, lastname: string, email:string, gender:string, salary: number) {
+  id: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  gender: string;
+  salary: number;
+
+  constructor(id: string, firstname: string, lastname: string, email:string, gender:string, salary: number) {
       this.id = id;
       this.firstname = firstname;
       this.lastname = lastname;
       this.email = email;
       this.gender = gender;
       this.salary = salary;
-    }
   }
+}
+
